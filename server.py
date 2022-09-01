a2a467b3612a05bc42e37d024dff3afe20d2d905
Vladoleg <shaporin.v@gmail.com>
2022-09-01 07:37:03 +0000

    testserver

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from pymongo import MongoClient
 from pprint import pprint
 import datetime as dt
@@ -9,15 +9,17 @@ my_db=client.cvedb
 collection =  my_db.cves
 
 #static variables
-fields=['id', 'assigner', 'Published', 'Modified', 'last-modified', 'summary', 'impactScore3', 'exploitabilityScore3', 'exploitabilityScore', 'impactScore', 'references', 'vendors', 'cwe', 'vulnerable_configuration_cpe_2_2']
+fields=['id', 'assigner', 'Published', 'Modified', 'last-modified', 'summary', 'impactScore', 'impactScore3', 'exploitabilityScore', 'exploitabilityScore3', 'references', 'vendors', 'cwe', 'vulnerable_configuration_cpe_2_2']
 now=dt.datetime.today()
 far=dt.datetime(2022, 7, 1)
 cve=[]
 elem=[]
+a = {}
 
 app = Flask(__name__)
 vendors = [{'name': 'huawei', 'url': 'huawei'},
            {'name': 'dell', 'url': 'dell'},
+           {'name': 'redhat', 'url': 'redhat'},
            {'name': 'vmware', 'url': 'vmware'},
            {'name': 'veeam', 'url': 'veeam'},
            {'name': 'netapp', 'url': 'netapp'},
@@ -29,33 +31,43 @@ vendors = [{'name': 'huawei', 'url': 'huawei'},
 
 #selecting last 15 documents for mainpage and vendors
 
-def last():
+
+
+def last15(vndr):
 	elem.clear()
-	for i in collection.find().limit(-15):
-		x = (i['id'])
-		elem.append( {'name': x, 'url': 'cveinfo/'+x })
-	return elem
+	elem2 = []
+	for i in collection.find({'vendors': vndr}, {'_id':0, 'id':1, 'Published':1, 'last-modified':1}).limit(3):
+		elem.append({'name': i['id'], 'url': 'cveinfo/'+i['id']})
+		elem2
 
 
+def last():
+	li = ['id', 'Published', 'last-modified']
+	for i in collection.find({}, {fields[0]: 1, fields[2]:1, fields[4]:1, '_id':0}).limit(3):
+#		a = {x: i[x]  for x in li}
+		print (i)
+
+last()
+'''
 def last_v(vendor):
 	elem.clear()
-	for i in collection.find({'vendors': vendor}).limit(-15):
-		x = i['id']
-		elem.append({'name': x, 'url': 'cveinfo/'+x})
+	for i in collection.find({'vendors': vendor}, {" last-modified": {"$slice": -15}}).limit(15):
+		elem.append({'name': i['id'], 'url': 'cveinfo/'+i['id']})
 	return elem
 
-# Main page with last 15 pieces
+# Main page with last 15 cve
 @app.route('/')
 def index():
 	last()
-	return render_template('index.html', vendors=vendors, elem=elem)
+	return render_template('index.html', vendors=vendors, e=a)
 
-#vendor page with 15 last pieces
+#vendor page with last 15 cve
 @app.route('/<vendor>')
 def vendor(vendor):
 	last_v(vendor)
-	return render_template('vendors.html', vendors=vendors, elem=elem)
+	return render_template('vendors.html', vendors=vendors, elem=elem, v=vendor)
 
+#table for cve ID
 @app.route('/cveinfo/<cveid>')
 def cvepage(cveid):
 	cve = collection.find_one({'id': cveid})
@@ -65,3 +77,4 @@ def cvepage(cveid):
 if __name__=="__main__":
 	app.run(debug=True, host='192.168.0.9', port=80)
 
+'''
